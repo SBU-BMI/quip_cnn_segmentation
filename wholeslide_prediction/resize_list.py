a6a73caff50881e6f0575bb7ebd82cc64af71700
scotthoule <lehou0312@gmail.com>
2018-09-24 11:24:35 -0400

    Update resize_list.py

--- wholeslide_prediction/resize_list.py
+++ wholeslide_prediction/resize_list.py
@@ -1,10 +1,9 @@
 import sys
 import os
 index=sys.argv[1]
-files=os.listdir('./segmentation_test_images_'+str(index))
-list_txt=open('./segmentation_test_images_'+str(index)+'/image_resize_list.txt','w')
+files=os.listdir('./'+index)
+list_txt=open('./'+index+'/image_resize_list.txt','w')
 for i in range(len(files)):
   if files[i][len(files[i])-3:len(files[i])]=='png':
     list_txt.write(files[i]+' 1\n')
 list_txt.close()
-  
