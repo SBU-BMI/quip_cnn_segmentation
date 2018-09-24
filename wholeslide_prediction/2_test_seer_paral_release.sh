c31dc82c8d9410f7ab0352f2e2685b7bf7cc4389
scotthoule <lehou0312@gmail.com>
2018-09-24 11:27:28 -0400

    Update 2_test_seer_paral_release.sh

--- wholeslide_prediction/2_test_seer_paral_release.sh
+++ wholeslide_prediction/2_test_seer_paral_release.sh
@@ -7,20 +7,23 @@
 DATASET=BC_378_1_1
 GPU=1
 
+# PAR_CODE: 0 ~ PAR_MAX-1
+PAR_CODE=0
+PAR_MAX=1
 
-INDEX=0
-STEP=10000
 MODEL=generative_2018-01-02_23-21-39
 
-mkdir segmentation_test_images_${DATASET}
-cp ./tiles/${DATASET}/*.png segmentation_test_images_${DATASET}/
-
-python resize_list.py ${DATASET}
+DATASET_FOLDER=segmentation_test_images_${DATASET}
+mkdir ${DATASET_FOLDER}
+cp ./tiles/${DATASET}/*.png ${DATASET_FOLDER}/
+python resize_list.py ${DATASET_FOLDER}
 
 # On eagle and lired, we installed an updated gclib
 CUDA_VISIBLE_DEVICES=${GPU} \
 LD_LIBRARY_PATH="/home/lehhou/my_libc_env/lib/x86_64-linux-gnu/:/home/lehhou/my_libc_env/usr/lib64/:$LD_LIBRARY_PATH" \
         /home/lehhou/my_libc_env/lib/x86_64-linux-gnu/ld-2.17.so /home/lehhou/anaconda2/bin/python -u main.py \
-        --is_train=False --load_path=${MODEL} --mao_step_size=${STEP} --mao_index=${INDEX} --mao_file=${DATASET} --pred_scaling=0.5&> log.wholeslide${DATASET}_${INDEX}.txt
+        --is_train=False --load_path=${MODEL} --par_code=${STEP} --par_max=${INDEX} \
+        --seg_path=./${DATASET_FOLDER}/ --pred_scaling=0.5 \
+        &> log.wholeslide_${DATASET}_${PAR_CODE}_${PAR_MAX}.txt
 
 exit 0
