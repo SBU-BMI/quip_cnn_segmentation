7b16d9f336a6a4583599496cb600465d4d77d21e
scotthoule <lehou0312@gmail.com>
2019-04-26 15:15:30 -0400

    Update draw_fake.sh

--- training-data-synthesis/draw_fake.sh
+++ training-data-synthesis/draw_fake.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Start 5 processes
 
-python draw_fake.py ${1} ${2}
+nohup python draw_fake.py 0 5 &
+nohup python draw_fake.py 1 5 &
+nohup python draw_fake.py 2 5 &
+nohup python draw_fake.py 3 5 &
+nohup python draw_fake.py 4 5 &
 
 exit 0
